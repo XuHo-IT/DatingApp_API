723462e81bf396235e47f80741a468d9e4dd3c9b
Xuho <ngotranxuanhoa09062004@gmail.com>
2025-04-17 16:15:00 +0700

    authenticate to signalIR

--- API/Program.cs
+++ API/Program.cs
@@ -58,8 +58,8 @@ app.UseSwaggerUI();
 
 // Configure the HTTP request pipeline.
 app.UseMiddleware<ExceptionMiddleware>();
-app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().
-WithOrigins("http://localhost:4200", "https://localhost:4200"));
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().AllowCredentials()
+.WithOrigins("http://localhost:4200", "https://localhost:4200"));
 
 app.UseAuthentication();
 app.UseAuthorization();
